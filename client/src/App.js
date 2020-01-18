fcedecaf8f4a3c8c308e5ad2488e01bf18358a21
Brandon Stephens <brandonstephens@me.com>
2020-01-18 13:31:40 -0500

    private route chapter 67

--- client/src/App.js
+++ client/src/App.js
@@ -7,6 +7,7 @@ import About from './components/pages/About'
 import Register from './components/auth/Register'
 import Login from './components/auth/Login'
 import setAuthToken from './utils/setAuthToken'
+import PrivateRoute from './components/routing/PrivateRoute'
 import './App.css'
 
 import ContactState from './context/contact/ContactState'
@@ -28,7 +29,7 @@ const App = () => {
               <div className="container">
                 <Alerts />
                 <Switch>
-                  <Route exact path="/" component={Home} />
+                  <PrivateRoute exact path="/" component={Home} />
                   <Route exact path="/about" component={About} />
                   <Route exact path="/register" component={Register} />
                   <Route exact path="/login" component={Login} />
