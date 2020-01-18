fe65c5dab50ba95685ef4ee2b36bdf45ac155ce6
Brandon Stephens <brandonstephens@me.com>
2020-01-18 15:01:15 -0500

    prep for deploy

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const connectDB = require('./config/db')
+const path = require('path')
 
 const app = express()
 
@@ -14,6 +15,13 @@ app.get('/', (request, response) => response.json({ msg: 'Welcome to the machine
 app.use('/api/auth', require('./routes/auth'))
 app.use('/api/users', require('./routes/users'))
 app.use('/api/contacts', require('./routes/contacts'))
+
+// Server static assets in production
+if (process.env.NODE_ENV === 'production') {
+  // set a static folder
+  app.use(express.static('/client/build'))
+  app.get('*', (request, response) => response.sendFile(path.resolve(__dirname, 'client', 'build', 'index')))
+}
 
 const PORT = process.env.PORT || 5000
 
